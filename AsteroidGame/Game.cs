dcb9ea744db46c88321d53ebf465e3c2d285c010
Vlad Kalinovskiy <ndm_siba@mail.ru>
2020-04-23 00:35:40 +0300

    Визуализация игровых обьектов

--- AsteroidGame/Game.cs
+++ AsteroidGame/Game.cs
@@ -12,6 +12,7 @@ namespace AsteroidGame
     {
         private static BufferedGraphicsContext __Context;
         private static BufferedGraphics __Buffer;
+        private static VisualObject[] __GameObjects;
         public static int Width { get; set; }
 
         public static int Height { get; set; }
@@ -29,10 +30,22 @@ namespace AsteroidGame
         {
             Graphics g = __Buffer.Graphics;
             g.Clear(Color.Black);
-            g.DrawRectangle(Pens.White, new Rectangle(50, 50, 200, 200));
-            g.FillRectangle(Brushes.Red, new Rectangle(100, 50, 70, 120));
+
+            foreach (var game_object in __GameObjects)
+                game_object.Draw(g);
 
             __Buffer.Render();
+        }
+        public static void Load()
+        {
+            __GameObjects = new VisualObject[30];
+            for (var i = 0; i < __GameObjects.Length; i++)
+                __GameObjects[i] = new VisualObject(new Point(600, i * 20), new Point(15 - i, 20 - i), new Size(20, 20));
+        }
+        public static void Update()
+        {
+            foreach (var game_object in __GameObjects)
+                game_object.Update();
         }
     }
 }
