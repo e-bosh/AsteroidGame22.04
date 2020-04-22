934bdf13b2126506a5bc728fad623c0719b36da4
Vlad Kalinovskiy <ndm_siba@mail.ru>
2020-04-22 23:06:01 +0300

    Пример описания класса.Вектор+операторы(сумма разность и приведение типов)

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -14,13 +14,22 @@ namespace TestConsole
             //player1.Name = "Vlad";
             //player1.Birthday = new DateTime(1996,28,07,0,0,0);
 
-            Player player1 = new Player("Empty", new DateTime(1996, 07, 28));
+            //Player player1 = new Player("Empty", new DateTime(1996, 07, 28));
 
-            Console.Write("Input Surname: ");
-            player1.Name = Console.ReadLine();
+            //Console.Write("Input Surname: ");
+            //player1.Name = Console.ReadLine();
 
 
-            Console.WriteLine(player1.Name);
+            //Console.WriteLine(player1.Name);
+            Vector2D v1 = new Vector2D(5,7);
+            Vector2D v2 = new Vector2D(-7,2);
+            Vector2D v3 = v1 + v2;
+            Vector2D v4 = v3 + 3.141592;
+
+            double pi = 3.1415;
+            int i = (int)pi;
+
+            double length = v4;  
             Console.ReadLine();
         }
     }
---
+++ Tests/TestConsole/Vector2D.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Vector2D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(X * X + Y * Y);
+            }
+        }
+
+        public double Angle => Math.Atan2(X, Y);
+
+
+        public Vector2D(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2D operator +(Vector2D a,Vector2D b)
+        {
+            return new Vector2D(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Vector2D operator -(Vector2D a, Vector2D b)
+        {
+            return new Vector2D(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static Vector2D operator +(Vector2D a, double x)
+        {
+            return new Vector2D(a.X + x, a.Y + x);
+        }
+
+        public static implicit operator double(Vector2D vector)
+        {
+            return vector;
+        }
+    }
+
+}
