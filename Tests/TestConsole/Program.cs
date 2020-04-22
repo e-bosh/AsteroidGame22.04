d6cbff617ce469b4e3cb705bfe3da4c84828025e
Vlad Kalinovskiy <ndm_siba@mail.ru>
2020-04-22 22:11:42 +0300

    Пример первого класса -Игрок

---
+++ Tests/TestConsole/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Player player1 = new Player();
+            //player1.Name = "Vlad";
+            //player1.Birthday = new DateTime(1996,28,07,0,0,0);
+
+            Console.Write("Input Surname: ");
+            var surname = Console.ReadLine();
+
+            Player player1 = new Player(surname, new DateTime(1996, 07, 28));
+
+
+            Console.WriteLine(player1.GetName());
+            Console.ReadLine();
+        }
+    }
+
+    internal class Player
+    {
+        private string _Name;
+        private DateTime _Birthday;
+
+        public string GetName()
+        {
+            return _Name;
+
+        }
+
+        public void SetName(string Name)
+        {
+            _Name = Name;
+        }
+
+        public Player()
+        {
+            
+        }
+
+        public Player(string Name)
+        {
+            this._Name = Name;
+            _Birthday = DateTime.Now;
+        }
+
+        public Player(string Name, DateTime Birthday)
+            : this(Name)
+        {
+            //this.Name = Name;
+            this._Birthday = Birthday;
+
+        }
+    }
+}
